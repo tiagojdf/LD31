3acb33a4f2b86e8648a912d87698d74fb50a6e2f
Tiagojdferreira <tiagojdferreira@gmail.com>
2014-12-06 00:34:37 +0100

    Trying to put melon.js up online

---
+++ boilerplate-master/js/resources.js
@@ -0,0 +1,31 @@
+game.resources = [
+
+    /* Graphics.
+     * @example
+     * {name: "example", type:"image", src: "data/img/example.png"},
+     */
+    //our level tileset
+    {name: "area01_level_tiles", type:"image", src:"data/img/map/area01_level_tiles.png"},
+
+    /* Texture Atlases
+     * @example
+     * {name: "texture", type: "json", src: "data/img/example_tps.json"},
+     */
+
+    /* Maps.
+     * @example
+     * {name: "example01", type: "tmx", src: "data/map/example01.tmx"},
+     * {name: "example01", type: "tmx", src: "data/map/example01.json"},
+      */
+    {name: "area01",type: "tmx", src: "data/map/area01.tmx"}
+
+    /* Background music.
+     * @example
+     * {name: "example_bgm", type: "audio", src: "data/bgm/"},
+     */
+
+    /* Sound effects.
+     * @example
+     * {name: "example_sfx", type: "audio", src: "data/sfx/"}
+     */
+];

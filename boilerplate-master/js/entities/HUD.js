e154c6d336560c9311b76e1f1bdc4d2ea74bc3a3
Tiagojdferreira <tiagojdferreira@gmail.com>
2014-12-09 23:28:38 +0100

    Add timer and score

--- boilerplate-master/js/entities/HUD.js
+++ boilerplate-master/js/entities/HUD.js
@@ -1,3 +1,4 @@
+/*global game, me, console*/
 /**
  * a HUD container and child items
  */
@@ -24,7 +25,8 @@ game.HUD.Container = me.Container.extend({
         this.name = "HUD";
 
         // add our child score object at the top left corner
-        this.addChild(new game.HUD.ScoreItem(5, 5));
+        this.addChild(new game.HUD.ScoreItem(32, 16));
+        this.addChild(new game.HUD.Timer(me.game.currentLevel.width, 16));
     }
 });
 
@@ -41,9 +43,15 @@ game.HUD.ScoreItem = me.Renderable.extend({
         // call the parent constructor
         // (size does not matter here)
         this._super(me.Renderable, 'init', [x, y, 10, 10]);
+        
+        //create a font
+        this.font  = new me.Font("SATYP___", 24, '#e0dedc', 'left');
 
         // local copy of the global score
         this.score = -1;
+        
+        // make sure we use screen coordinates
+        this.floating = true;
     },
 
     /**
@@ -62,8 +70,57 @@ game.HUD.ScoreItem = me.Renderable.extend({
     /**
      * draw the score
      */
-    draw : function (context) {
+    draw : function (renderable) {
         // draw it baby !
+        this.font.draw(renderable.getContext() , "Slave: " + game.data.score +"/14", this.pos.x, this.pos.y);
     }
-
 });
+
+/**
+ * a basic HUD item to display time
+ */
+game.HUD.Timer = me.Renderable.extend({
+    /**
+     * constructor
+     */
+    init: function(x, y) {
+
+        // call the parent constructor
+        // (size does not matter here)
+        this._super(me.Renderable, 'init', [x, y, 10, 10]);
+        
+        //create a font
+        this.font  = new me.Font("SATYP___", 24, '#e0dedc', 'right');
+
+        // local copy of the global score
+        this.start = new Date().getTime();
+        this.time = 0;
+        
+        // make sure we use screen coordinates
+        this.floating = true;
+    },
+
+    /**
+     * update function
+     */
+    update : function () {
+        // we don't do anything fancy here, so just
+        // return true if the score has been updated
+        //var this.time = this.init.getMilliseconds();
+        this.time = new Date().getTime() - this.start;
+        return true;
+    },
+
+    /**
+     * draw the score
+     */
+    draw : function (renderable) {
+        // draw it baby !
+        this.font.draw(renderable.getContext() ,"TIME: " + (Math.floor(this.time/(1000*60)))+ ":" + pad((Math.floor((this.time/1000)%60)),2)+ ":" + pad((this.time % 1000), 3), this.pos.x, this.pos.y);
+    }
+});
+
+function pad(num, size) {
+    var s = "000000000" + num;
+    return s.substr(s.length-size);
+}
